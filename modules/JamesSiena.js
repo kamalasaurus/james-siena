e17f66f6bb3bf49c202a9ebc81e16e1c8794b38d
kamalasaurus <kamalasaurus@gmail.com>
2018-12-31 03:08:58 -0500

    iterating on transformation api

--- modules/JamesSiena.js
+++ modules/JamesSiena.js
@@ -1,12 +1,12 @@
 export default function JamesSiena(L = 500) {
   let colors = ['royalblue', 'magenta', 'cornsilk', 'chocolate'];
 
-  let toRad = (deg) => deg * Math.PI / 180;
+  let toRad = (deg) => { return deg * Math.PI / 180; };
 
   return function rectangle(
     context,
-    x = 0,
-    y = 0,
+    x = document.body.clientWidth / 3,
+    y = document.body.clientHeight / 3,
     length = L,
     color = 0,
     generation = 0
@@ -17,11 +17,14 @@ export default function JamesSiena(L = 500) {
     let nextcolor = (color + 1) % 4;
     let nextgen = generation + 1
 
-    context.fillRect(x, y, length, length/2);
-    rectangle(context, x, y, length/2, nextcolor, 0);
+    //context.translate(length/2, -2*length);
 
-    //context.rotate
-    //context.translate
+    context.translate(length/3, length/6);
+    context.rotate(toRad(-90 * generation)); // skip first generation
+
+    context.fillRect(x, y, length, length/2);
+    context.resetTransform();
+    rectangle(context, x, y, length/2, nextcolor, nextgen);
 
     switch (generation) {
       case 0:
